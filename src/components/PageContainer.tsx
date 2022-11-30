ea9a7fcfc45c7f5109a07a9d7401e370fec69542
Pal Kerecsenyi <pal@p.kerecs.com>
2022-11-30 23:29:08 +0000

    Add festive styling

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,10 +1,15 @@
 import { ReactNode } from "react"
+import styles from "../styles/tinsel.module.scss"
 
 type props = {
     children: ReactNode
 }
 export default function PageContainer({children}: props) {
-    return <div className="container py-6 px-4">
-        {children}
+    return <div>
+        <div className={styles.tinselContainer} />
+
+        <div className="container py-4 px-4">
+            {children}
+        </div>
     </div>
 }
--- src/pages/Trip.tsx
+++ src/pages/Trip.tsx
@@ -5,6 +5,8 @@ import { useStripeStatus } from "../data/stripe"
 import { useCallback } from "react"
 import { getAuth, signOut } from "firebase/auth"
 import PageContainer from "../components/PageContainer"
+import {ReactComponent as Gnome} from "../assets/Gnome1.svg"
+import {ReactComponent as Snowman} from "../assets/Snowman.svg"
 
 export default function Trip() {
     const trip = useTripContext()
@@ -28,58 +30,78 @@ export default function Trip() {
             Current shopping trip
         </h1>
 
-        <p>
-            The deadline for order submissions is <strong>{timestampFormat(trip.itemsDeadline)}</strong>.
-        </p>
-        <p>
-            After this time, payments will open. Please make payments by <strong>{timestampFormat(trip.paymentDeadline)}</strong>.
-        </p>
+        <div className="columns is-5 is-variable is-align-items-flex-end">
+            <div className="column">
+                <div className="columns">
+                    <div className="column">
+                        <p>
+                            The deadline for order submissions is <strong>{timestampFormat(trip.itemsDeadline)}</strong>.
+                        </p>
+                        <p>
+                            After this time, payments will open. Please make payments by <strong>{timestampFormat(trip.paymentDeadline)}</strong>.
+                        </p>
 
-        {status && <p>
-            Your invoices will be emailed to <strong>{status.email}</strong>.
-        </p>}
+                        {status && <p>
+                            Your invoices will be emailed to <strong>{status.email}</strong>.
+                        </p>}
+                    </div>
+                    <div className="column is-flex-grow-0">
+                        <Snowman
+                            width={60}
+                        />
+                    </div>
+                </div>
 
-        {status && status.balance !== 0 && <article className={`message mt-4 ${status.balance > 0 ? 'is-success' : 'is-danger'}`}>
-            <div className="message-body">
-                <p>
-                    <strong>You have a balance!</strong>
-                </p>
-                <p>
-                    Your account has a balance of <strong>{currencyFormat(status.balance)}</strong>. This
-                    will be applied as a {status.balance > 0 ? 'discount' : 'surcharge'} to your next order.
-                </p>
-                {status.balance > 0 && <p>
-                    This might be because an item in a previous order got substituted for a cheaper alternative.
-                </p>}
-                {status.balance < 0 && <p>
-                    This might be because you were accidentally missing a charge in your last order or because Pal
-                    added a debit to your account for some reason.
-                </p>}
-                <p>
-                    If this balance doesn't make sense, please ask Pal well before the order deadline. He makes mistakes sometimes.
-                </p>
-            </div>
-        </article>}
+                {status && status.balance !== 0 && <article className={`message mt-2 ${status.balance > 0 ? 'is-success' : 'is-danger'}`}>
+                    <div className="message-body">
+                        <p>
+                            <strong>You have a balance!</strong>
+                        </p>
+                        <p>
+                            Your account has a balance of <strong>{currencyFormat(status.balance)}</strong>. This
+                            will be applied as a {status.balance > 0 ? 'discount' : 'surcharge'} to your next order.
+                        </p>
+                        {status.balance > 0 && <p>
+                            This might be because an item in a previous order got substituted for a cheaper alternative.
+                        </p>}
+                        {status.balance < 0 && <p>
+                            This might be because you were accidentally missing a charge in your last order or because Pal
+                            added a debit to your account for some reason.
+                        </p>}
+                        <p>
+                            If this balance doesn't make sense, please ask Pal well before the order deadline. He makes mistakes sometimes.
+                        </p>
+                    </div>
+                </article>}
 
-        <article className={`message mt-4 ${trip.status === TripStatus.AcceptingOrders ? 'is-success' : 'is-info'}`}>
-            <div className="message-header">
-                <p>Order status</p>
+                <article className={`message mt-2 ${trip.status === TripStatus.AcceptingOrders ? 'is-success' : 'is-info'}`}>
+                    <div className="message-header">
+                        <p>Order status</p>
+                    </div>
+                    <div className="message-body">
+                        <p>
+                            This shopping trip is currently <strong>{formatTripStatus(trip.status)}</strong>.
+                        </p>
+                        {trip.status === TripStatus.AcceptingOrders && <p>
+                            You can use the button below to register your cart contents.
+                        </p>}
+                        {trip.status === TripStatus.OrderPlacedAwaitingDelivery && <p>
+                            Delivery is scheduled for <strong>{timestampFormat(trip.delivery)}</strong>.
+                        </p>}
+                        {trip.status === TripStatus.Complete && <p>
+                            The next shopping trip will be opened soon! Please ask Pal if you really need it open now.
+                        </p>}
+                    </div>
+                </article>
             </div>
-            <div className="message-body">
-                <p>
-                    This shopping trip is currently <strong>{formatTripStatus(trip.status)}</strong>.
-                </p>
-                {trip.status === TripStatus.AcceptingOrders && <p>
-                    You can use the button below to register your cart contents.
-                </p>}
-                {trip.status === TripStatus.OrderPlacedAwaitingDelivery && <p>
-                    Delivery is scheduled for <strong>{timestampFormat(trip.delivery)}</strong>.
-                </p>}
-                {trip.status === TripStatus.Complete && <p>
-                    The next shopping trip will be opened soon! Please ask Pal if you really need it open now.
-                </p>}
+            <div
+                className="column is-flex-grow-0"
+            >
+                <Gnome
+                    width={70}
+                />
             </div>
-        </article>
+        </div>
 
         <div className="buttons">
             {trip.status === TripStatus.AcceptingOrders && <Link
