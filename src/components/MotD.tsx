e0d36ee010fff7a45eb7cd83e6683350ba952f68
Pal Kerecsenyi <pal@p.kerecs.com>
2026-01-06 21:32:28 +0100

    Remove MOTD

--- src/components/MotD.tsx
+++ src/components/MotD.tsx
@@ -1,6 +1,6 @@
-import ReactMarkdown from "react-markdown"
+// import ReactMarkdown from "react-markdown"
 import { useAppSelector } from "../stores/hooks"
-import styles from "../styles/markdown.module.scss"
+// import styles from "../styles/markdown.module.scss"
 
 export default function MotD() {
     const motd = useAppSelector(state => state.tripsReducer.motd)
@@ -8,6 +8,9 @@ export default function MotD() {
         return <></>
     }
 
+    return <></>
+
+    /**
     return <article className="message">
         <div className="message-header">
             <p>Message</p>
@@ -20,4 +23,5 @@ export default function MotD() {
             </div>
         </div>
     </article>
+    **/
 }
